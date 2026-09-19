9762a2f5a4e549acc7acc7740250bfa605966a39
April F <ferrisapril45@byui.edu>
2026-09-18 22:59:37 -0500

    Update greeting to prompt for user names
    commit change to prep1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
