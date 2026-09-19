135bd4ce021d6e4e0de58505c5eb440b338a74cf
April F <ferrisapril45@byui.edu>
2026-09-18 23:09:25 -0500

    revert

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
-        string first = Console.ReadLine();
-
-        Console.WriteLine("What is your last name?");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        Console.WriteLine("Hello Learning02 World!");
     }
 }
