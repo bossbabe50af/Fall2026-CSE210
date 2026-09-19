d3d863c5e3550f099987595ba0e68ea92d471144
bossbabe50afgit config --global user.email <godsgyrl46@gmail.com>
2026-09-18 22:45:12 -0500

    commit changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade average?");
+        string answer = Console.ReadLine();
+        int average = int.Parse(answer);
+
+        string letter = "";
+
+        if (average >= 90)
+        { letter = "A"; }
+
+        else if (average >= 80)
+        { letter = "B"; }
+
+        else if (average >= 70)
+        { letter = "C"; }
+
+        else if (average >= 60)
+        { letter = "D"; }
+
+        else
+        { letter = "F"; }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (average >= 70)
+
+            Console.WriteLine("You passed!");
+
+        else
+        {
+            Console.WriteLine("You have not made it yet!");
+        }
+
+
     }
 }
