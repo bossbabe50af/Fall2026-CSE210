b8b4a4acc6877a1a257890fd76f80533003c3ff4
April F <ferrisapril45@byui.edu>
2026-09-24 10:30:23 -0500

    complete prep push 3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        int userMagicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers =new List<int>();
+        Console.WriteLine("Enter list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        do
+        {
+            //ask for number
+        Console.Write("Enter number: ");
+        number = int.Parse(Console.ReadLine());
+
+        if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        
+    }
+           while (number != 0);
+
+            int sum = 0;
+
+        foreach (int numberInList in numbers)
+            {
+                sum += numberInList;
+            }
+
+        
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int currentNumber in numbers)
+        {
+            if (currentNumber > max)
+            {max = currentNumber;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
